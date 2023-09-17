116b343bb852a57c5ccf94c244d627cc9b8c5e24
Ritwik <ritwikkumarrishu@gmail.com>
2023-09-17 19:53:18 +0530

    Fix: object destructuring variable name mismatch

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,11 +2,11 @@ import Order from "../model/orderSchema.js";
 
 async function createOrder(req, res) {
   try {
-    const { orderDetail, userId, totalPrice } = req.body;
-    const products = orderDetail.map((item) => {
+    const { data: orderDetails, userId, totalAmt } = req.body;
+    const products = orderDetails.map((item) => {
       return { productId: item._id, price: item.price };
     });
-    const data = await Order.create({ products, userId, amount: totalPrice });
+    const data = await Order.create({ products, userId, amount: totalAmt });
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json(error);
