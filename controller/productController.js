cb75ac86c406f85dc8b2499f610848883362d6a7
Ritwik <ritwikkumarrishu@gmail.com>
2024-02-12 20:56:13 +0530

    feat: added async handler fix: used async handler in controller

--- controller/productController.js
+++ controller/productController.js
@@ -1,131 +1,108 @@
+import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../model/productSchema.js";
 import User from "../model/userSchema.js";
 
-async function getProducts(req, res) {
-  try {
-    const page = Number(req.query.page) || 1;
-    const pageSize = Number(req.query.pageSize) || 10;
+const getProducts = asyncHandler(async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const pageSize = Number(req.query.pageSize) || 10;
 
-    const skip = (page - 1) * pageSize;
+  const skip = (page - 1) * pageSize;
 
-    const products = await Product.find().skip(skip).limit(pageSize);
-    const totalCount = await Product.countDocuments();
+  const products = await Product.find().skip(skip).limit(pageSize);
+  const totalCount = await Product.countDocuments();
 
-    res.json({
-      products,
-      page,
-      pageSize,
-      totalCount,
-    });
-  } catch (error) {
-    res.status(401).json(error);
-  }
-}
+  res.json({
+    products,
+    page,
+    pageSize,
+    totalCount,
+  });
+});
 
-async function getSingleProduct(req, res) {
-  try {
-    const id = req.params.id;
-    const data = await Product.findById(id)
-      .populate({
-        path: "ratingsAndReviews.ratingAndReview",
-        populate: {
-          path: "userId",
-          model: "User",
-          select: "name",
-        },
-      })
-      .exec();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-}
+const getSingleProduct = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const data = await Product.findById(id)
+    .populate({
+      path: "ratingsAndReviews.ratingAndReview",
+      populate: {
+        path: "userId",
+        model: "User",
+        select: "name",
+      },
+    })
+    .exec();
+  res.status(200).json(data);
+});
 
-async function addProduct(req, res) {
-  try {
-    const data = await Product.create(req.body);
-    res.status(201).json(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const addProduct = asyncHandler(async (req, res) => {
+  const data = await Product.create(req.body);
+  res.status(201).json(data);
+});
 
 // GET: Featured Product
-async function getFeaturedProduct(req, res) {
-  try {
-    const data = await Product.find({ featured: true });
-    res.status(200).json(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const getFeaturedProduct = asyncHandler(async (req, res) => {
+  const data = await Product.find({ featured: true });
+  res.status(200).json(data);
+});
 
 // POST: Give ratings
-async function ratingsAndReviews(req, res) {
-  try {
-    const { rating: ratings, productId, review } = req.body;
-    const product = await Product.findById(productId);
+const ratingsAndReviews = asyncHandler(async (req, res) => {
+  const { rating: ratings, productId, review } = req.body;
+  const product = await Product.findById(productId);
 
-    if (!product) {
-      return res.status(400).json("Product Not Found");
-    }
-
-    // Convert req.user to ObjectId type
-    const { userId } = req.userData;
-
-    const { name } = await User.findById(userId);
-
-    // Add the new rating to the product's ratingsAndReviews
-    product.ratingsAndReviews.ratingAndReview.push({
-      userId: { _id: req.userData.userId, name },
-      ratings,
-      review,
-    });
-
-    // Save the updated product with the new rating
-    const updatedProduct = await product.save();
-
-    // Calculate the updated total ratings and update it in the product document
-    updatedProduct.ratingsAndReviews.totalRatings =
-      updatedProduct.ratingsAndReviews.ratingAndReview.length;
-
-    await updatedProduct.save();
-
-    res.status(200).json({
-      success: true,
-      message: "Rating added successfully",
-      ratingsAndReviews: updatedProduct.ratingsAndReviews,
-    });
-  } catch (error) {
-    res.status(400).json(error);
+  if (!product) {
+    return res.status(400).json("Product Not Found");
   }
-}
+
+  // Convert req.user to ObjectId type
+  const { userId } = req.userData;
+
+  const { name } = await User.findById(userId);
+
+  // Add the new rating to the product's ratingsAndReviews
+  product.ratingsAndReviews.ratingAndReview.push({
+    userId: { _id: req.userData.userId, name },
+    ratings,
+    review,
+  });
+
+  // Save the updated product with the new rating
+  const updatedProduct = await product.save();
+
+  // Calculate the updated total ratings and update it in the product document
+  updatedProduct.ratingsAndReviews.totalRatings =
+    updatedProduct.ratingsAndReviews.ratingAndReview.length;
+
+  await updatedProduct.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Rating added successfully",
+    ratingsAndReviews: updatedProduct.ratingsAndReviews,
+  });
+});
 
 // DELETE: Delete Ratings and review
-async function deleteRatingAndReview(req, res) {
-  try {
-    const { productID, userID } = req.body;
-    const product = await Product.findById(productID);
+const deleteRatingAndReview = asyncHandler(async (req, res) => {
+  const { productID, userID } = req.body;
+  const product = await Product.findById(productID);
 
-    const { ratingsAndReviews } = product;
-    const { ratingAndReview } = ratingsAndReviews;
+  const { ratingsAndReviews } = product;
+  const { ratingAndReview } = ratingsAndReviews;
 
-    // Filter out the object with the specified userID
-    const updatedRatingAndReview = ratingAndReview.filter(
-      (review) => review.userId._id.toString() !== userID
-    );
+  // Filter out the object with the specified userID
+  const updatedRatingAndReview = ratingAndReview.filter(
+    (review) => review.userId._id.toString() !== userID
+  );
 
-    // Update the ratingsAndReviews with the modified ratingAndReview array
-    ratingsAndReviews.ratingAndReview = updatedRatingAndReview;
+  // Update the ratingsAndReviews with the modified ratingAndReview array
+  ratingsAndReviews.ratingAndReview = updatedRatingAndReview;
 
-    // Save the changes to the product
-    await product.save();
+  // Save the changes to the product
+  await product.save();
 
-    res.status(200).json(ratingsAndReviews);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-}
+  res.status(200).json(ratingsAndReviews);
+});
 
 export {
   getProducts,
--- controller/userController.js
+++ controller/userController.js
@@ -2,213 +2,178 @@ import bcrypt from "bcryptjs";
 
 import User from "../model/userSchema.js";
 import sendPasswordResetEmail from "../middleware/sendMail.js";
+import asyncHandler from "../middleware/asyncHandler.js";
 
-async function login(req, res) {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(500).json({ error: "Please fill all details" });
-    }
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res
-        .status(500)
-        .json({ error: "User not registered!. Please register first" });
-    }
-    const isPasswordCorrect = await user.comparePassword(password);
-    if (!isPasswordCorrect) {
-      return res.status(500).json({ error: "Email or Password incorrect" });
-    }
-    const token = user.createJWT();
-    user.password = undefined;
-    res.status(200).json({ user, token });
-  } catch (error) {
-    res.status(500).json(error);
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(500).json({ error: "Please fill all details" });
   }
-}
-
-async function signup(req, res) {
-  try {
-    const { name, email, password, confirmPassword } = req.body;
-    if (!name || !email || !password) {
-      return res.status(500).json({ error: "Please fill all details" });
-    }
-    if (password !== confirmPassword) {
-      return res.status(500).json({ error: "Passwords do not match" });
-    }
-    const isEmailPresent = await User.findOne({ email });
-    if (isEmailPresent) {
-      return res
-        .status(500)
-        .json({ error: "You are already registered. Please log in" });
-    }
-    const user = await User.create({ name, email, password });
-    const token = user.createJWT();
-    user.password = undefined;
-    res.status(201).json({ user, token });
-  } catch (error) {
-    res.status(500).json(error);
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res
+      .status(500)
+      .json({ error: "User not registered!. Please register first" });
   }
-}
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    return res.status(500).json({ error: "Email or Password incorrect" });
+  }
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(200).json({ user, token });
+});
 
-async function addAddress(req, res) {
-  try {
-    const { userAddress, userId } = req.body;
-    const isUserPresent = await User.findById(userId);
-    if (!isUserPresent) {
-      return res.status(500).json({ error: "Email not present" });
-    }
+const signup = asyncHandler(async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  if (!name || !email || !password) {
+    return res.status(500).json({ error: "Please fill all details" });
+  }
+  if (password !== confirmPassword) {
+    return res.status(500).json({ error: "Passwords do not match" });
+  }
+  const isEmailPresent = await User.findOne({ email });
+  if (isEmailPresent) {
+    return res
+      .status(500)
+      .json({ error: "You are already registered. Please log in" });
+  }
+  const user = await User.create({ name, email, password });
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(201).json({ user, token });
+});
 
-    const user = await User.findByIdAndUpdate(
-      userId,
-      {
-        $push: {
-          address: {
-            name: userAddress.name,
-            phoneNumber: userAddress.phoneNumber,
-            address: userAddress.address,
-            city: userAddress.city,
-            state: userAddress.state,
-            postal: userAddress.postal_code,
-            country: userAddress.country,
-          },
+const addAddress = asyncHandler(async (req, res) => {
+  const { userAddress, userId } = req.body;
+  const isUserPresent = await User.findById(userId);
+  if (!isUserPresent) {
+    return res.status(500).json({ error: "Email not present" });
+  }
+
+  const user = await User.findByIdAndUpdate(
+    userId,
+    {
+      $push: {
+        address: {
+          name: userAddress.name,
+          phoneNumber: userAddress.phoneNumber,
+          address: userAddress.address,
+          city: userAddress.city,
+          state: userAddress.state,
+          postal: userAddress.postal_code,
+          country: userAddress.country,
         },
       },
-      { new: true }
-    );
-    const token = user.createJWT();
-    return res.status(200).json({ user, token });
-  } catch (error) {
-    return res.status(500).json({ message: error });
+    },
+    { new: true }
+  );
+  const token = user.createJWT();
+  return res.status(200).json({ user, token });
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  let { name, email, password, id } = req.body;
+  const data = await User.findById(id);
+  if (!data) {
+    return res.status(500).json({ error: "Email not present" });
   }
-}
+  const bodyToUpdate = { name, email };
+  const user = await User.findByIdAndUpdate(id, bodyToUpdate, {
+    new: true,
+  });
+  const token = user.createJWT();
+  return res.status(200).json({ user, token });
+});
 
-async function updateUser(req, res) {
-  try {
-    let { name, email, password, id } = req.body;
-    const data = await User.findById(id);
-    if (!data) {
-      return res.status(500).json({ error: "Email not present" });
-    }
-    const bodyToUpdate = { name, email };
-    const user = await User.findByIdAndUpdate(id, bodyToUpdate, {
-      new: true,
-    });
-    const token = user.createJWT();
-    return res.status(200).json({ user, token });
-  } catch (error) {
-    return res.status(500).json({ message: error });
+const updatePassword = asyncHandler(async (req, res) => {
+  const { passwordDetails, id } = req.body;
+  const isUserPresent = await User.findById(id);
+  const isPasswordCorrect = await isUserPresent.comparePassword(
+    passwordDetails.currPassword
+  );
+  if (!isPasswordCorrect) {
+    return res.status(500).json({ message: "Old Password Incorrect" });
   }
-}
+  const salt = await bcrypt.genSalt(10);
+  passwordDetails.newPassword = await bcrypt.hash(
+    passwordDetails.newPassword,
+    salt
+  );
+  const user = await User.findByIdAndUpdate(
+    id,
+    {
+      password: passwordDetails.newPassword,
+    },
+    { new: true }
+  );
+  user.password = undefined;
+  const token = await user.createJWT();
+  return res.status(200).json({ user, token });
+});
 
-async function updatePassword(req, res) {
-  try {
-    const { passwordDetails, id } = req.body;
-    const isUserPresent = await User.findById(id);
-    const isPasswordCorrect = await isUserPresent.comparePassword(
-      passwordDetails.currPassword
-    );
-    if (!isPasswordCorrect) {
-      return res.status(500).json({ message: "Old Password Incorrect" });
-    }
-    const salt = await bcrypt.genSalt(10);
-    passwordDetails.newPassword = await bcrypt.hash(
-      passwordDetails.newPassword,
-      salt
-    );
-    const user = await User.findByIdAndUpdate(
-      id,
-      {
-        password: passwordDetails.newPassword,
-      },
-      { new: true }
-    );
-    user.password = undefined;
-    const token = await user.createJWT();
-    return res.status(200).json({ user, token });
-  } catch (error) {
-    res.status(500).json(error);
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { user } = req.body;
+  const isUserPresent = await User.findOne({ email: user });
+  if (!isUserPresent) {
+    return res.status(400).json("Email Not Present");
   }
-}
+  const token = await isUserPresent.createJWT();
 
-async function forgotPassword(req, res) {
-  try {
-    const { user } = req.body;
-    const isUserPresent = await User.findOne({ email: user });
-    if (!isUserPresent) {
-      return res.status(400).json("Email Not Present");
-    }
-    const token = await isUserPresent.createJWT();
-
-    const resetLink = `https://myproshop.netlify.app/reset-password?token=${token}`;
-    // const resetLink = `http://localhost:3000/reset-password?token=${token}`;
-    const emailSent = await sendPasswordResetEmail(
-      isUserPresent.email,
-      resetLink
-    );
-    if (!emailSent) {
-      return res.status(400).json("Error");
-    }
-    res.status(200).json("Email Sent Successfully");
-  } catch (error) {
-    console.log(error);
+  const resetLink = `https://myproshop.netlify.app/reset-password?token=${token}`;
+  // const resetLink = `http://localhost:3000/reset-password?token=${token}`;
+  const emailSent = await sendPasswordResetEmail(
+    isUserPresent.email,
+    resetLink
+  );
+  if (!emailSent) {
+    return res.status(400).json("Error");
   }
-}
+  res.status(200).json("Email Sent Successfully");
+});
 
-async function deleteAddress(req, res) {
-  try {
-    const { userId, addressId } = req.query;
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(400).json("User not present");
-    }
-
-    // Find the index of the address in the user's address array
-    const addressIndex = user.address.findIndex(
-      (address) => address._id.toString() === addressId
-    );
-
-    if (addressIndex === -1) {
-      return res.status(400).json("Address not found");
-    }
-
-    // Remove the address from the address array
-    user.address.splice(addressIndex, 1);
-
-    // Save the updated user
-    await user.save();
-
-    return res.status(200).json(user);
-  } catch (error) {
-    console.log(error);
+const deleteAddress = asyncHandler(async (req, res) => {
+  const { userId, addressId } = req.query;
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(400).json("User not present");
   }
-}
 
-async function resetPassword(req, res) {
-  try {
-    const { newPassword } = req.body;
-    const user = await User.findById(req.user);
-    if (!user) {
-      return res.status(400).json("User not found");
-    }
+  // Find the index of the address in the user's address array
+  const addressIndex = user.address.findIndex(
+    (address) => address._id.toString() === addressId
+  );
 
-    // Update the password
-    user.password = newPassword;
-    await user.save();
-
-    res.status(200).json("Password reset successfully");
-  } catch (error) {
-    console.log(error);
+  if (addressIndex === -1) {
+    return res.status(400).json("Address not found");
   }
-}
 
-async function checkJWTExpiry(req, res) {
-  try {
-    res.status(200).json(true);
-  } catch (error) {
-    console.log(error);
+  // Remove the address from the address array
+  user.address.splice(addressIndex, 1);
+
+  // Save the updated user
+  await user.save();
+
+  return res.status(200).json(user);
+});
+
+const resetPassword = asyncHandler(async (req, res) => {
+  const { newPassword } = req.body;
+  const user = await User.findById(req.user);
+  if (!user) {
+    return res.status(400).json("User not found");
   }
-}
+
+  // Update the password
+  user.password = newPassword;
+  await user.save();
+
+  res.status(200).json("Password reset successfully");
+});
+
+const checkJWTExpiry = asyncHandler(async (req, res) => {
+  res.status(200).json(true);
+});
 
 export {
   login,
